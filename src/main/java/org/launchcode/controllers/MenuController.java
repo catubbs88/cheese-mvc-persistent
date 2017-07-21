3d1059784ff7fbdce12f236c8164f13ce46d673d
Constance <catubbs88@yahoo.com>
2017-07-21 13:17:44 -0500

    Able to add and name menus, fixed link urls

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by catub on 7/20/2017.
@@ -26,5 +32,24 @@ public class MenuController {
         model.addAttribute("menus", menuDao.findAll());
         model.addAttribute("title", "My Menus");
         return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "menu/add";
+        } else {
+            menuDao.save(menu);
+            return "redirect:view/" + menu.getId();
+        }
+
     }
 }
