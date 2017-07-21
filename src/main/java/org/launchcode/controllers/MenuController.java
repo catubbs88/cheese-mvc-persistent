a7abc7dd3f9481365878745fa58c63248eb67c37
Constance <catubbs88@yahoo.com>
2017-07-21 11:34:30 -0500

    MenuController set up to display menu names and links at /menu

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,30 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by catub on 7/20/2017.
+ */
+@Controller
+@RequestMapping(value="menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "My Menus");
+        return "menu/index";
+    }
+}
